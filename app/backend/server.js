52b898f291f69d614379bbd2eb6b4703679d2563
decenthasoo <decenthasoo@gmail.com>
2025-07-09 00:54:17 +0530

    Update server.js

--- app/backend/server.js
+++ app/backend/server.js
@@ -496,7 +496,7 @@ app.use((err, req, res, next) => {
 });
 
 // Server Start
-  const server = app.listen(PORT, '0.0.0.0', () => {
+const server = app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on port ${PORT}`);
   console.log('\n\x1b[36m=== Server Successfully Started ===\x1b[0m');
   console.log(`\x1b[32mPort:\x1b[0m ${PORT}`);
